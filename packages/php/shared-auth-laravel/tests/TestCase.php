83f53dc2a23e0770b8af29bba8d0a81ad11882bd
ggarrido <g.garridoportes@edu.gva.es>
2026-06-12 04:34:09 +0000

    feat(shared-auth-laravel): JwtSubject, ResolvesJwtUser y JwtProfileDto base
    Unifica la resolución del usuario JWT reimplementada en 19 archivos de 5 apps.
    - JwtSubject::fromRequest/claims sobre el atributo jwt_user de JwtMiddleware
    - Trait ResolvesJwtUser con modelo configurable (jwtUserModel(), evita gotcha PHP 8.4 abstract props)
    - JwtProfileDto readonly extensible (claims completos; dms extenderá los académicos)
    - Fixes de tests preexistentes: jwt_issuer del fixture y JwksServiceInterface en test de kid desconocido

--- packages/php/shared-auth-laravel/tests/TestCase.php
+++ packages/php/shared-auth-laravel/tests/TestCase.php
@@ -17,7 +17,7 @@ abstract class TestCase extends OrchestraTestCase
     protected function defineEnvironment($app): void
     {
         $app['config']->set('auth.jwks_url', 'http://keycloak.test/realms/maya/protocol/openid-connect/certs');
-        $app['config']->set('auth.jwt_issuer', 'https://keycloak.maya.test/realms/maya');
+        $app['config']->set('auth.jwt_issuer', 'http://keycloak.localhost/realms/maya');
         $app['config']->set('auth.jwt_audience', 'maya-test-service');
         $app['config']->set('auth.jwks_cache_ttl', 3600);
         $app['config']->set('auth.dev_bypass_auth', false);
